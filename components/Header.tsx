94bf89a1a9b16d2cc893bc5991e29cca9603292c
Srihari <76058816+Srihari1176@users.noreply.github.com>
2025-12-17 23:27:17 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = async ({ user }: { user: User }) => {
             <div className="container header-wrapper">
                 <Link href="/" className="flex items-center justify-center gap-2">
                       <Image
-                        src="https://image.pngaaa.com/216/4339216-middle.png"
+                        src="/assets/icons/star.svg" 
                         alt="MarketForge"
                         width={200}
                         height={50}
